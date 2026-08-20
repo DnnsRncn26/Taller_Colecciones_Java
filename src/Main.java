f5ea805bfbe7a4ce6a15f18c660b4bd0e83b1047
Dannis Dayana Rincon <dannis.rincon1997@gmail.com>
2026-08-19 22:17:05 -0500

    taller

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,86 @@
 public class Main {
     public static void main(String[] args) {
+        SistemaBiblioteca biblioteca = new SistemaBiblioteca(); //aqui se esta creando un objeto de la biblioteca
 
 
-    }
+    Libro libro1 = new Libro(
+            "BK-001",
+            "Erase una vez",
+            2026,
+            false,  //false quiere decir que el libro no esta disponible, esta prestado
+            "Shirley Lopez",
+            "20142911"
+    );
+
+    Libro libro2 = new Libro(
+            "BK-002",
+            "Las aventuras de tintin",
+            1997,
+            true,
+            "Yeimi Martinez",
+            "20141111"
+    );
+
+    Revista revista1 = new Revista(
+            "RV-001",
+            "Las hermanas calle",
+            2020,
+            true,
+            160,
+            "Farándula"
+    );
+
+    Revista revista2 = new Revista(
+            "RV-002",
+            "Los chismes de hoy",
+            2026,
+            true,
+            210,
+            "Comedia"
+    );
+
+        biblioteca.registrarMaterial(libro1);
+        biblioteca.registrarMaterial(libro2);
+        biblioteca.registrarMaterial(revista1);
+        biblioteca.registrarMaterial(revista2);
+
+        System.out.println("Catálogo");
+        biblioteca.mostrarCatalogo();
+
+        System.out.println("\n Buscar material");
+
+        Material encontrado = biblioteca.buscarMaterial("BK-001");  //aqui quiere decir busca en HashMap el material que tenga este codigo
+
+        if (encontrado != null) {  //se pregunta si encontro algo o no
+            encontrado.mostrarInfo();
+        } else {
+            System.out.println("Material no encontrado");
+        }
+
+        Material noEncontrado = biblioteca.buscarMaterial("BK-12345");  //se pregumta si se encontro este codigo
+
+        if (noEncontrado != null) {
+            noEncontrado.mostrarInfo();
+        } else {
+            System.out.println("Material no encontrado");
+        }
+
+        System.out.println("\n Lista de espera");
+
+        biblioteca.agregarEspera("BK-001", "Gloria"); //aqui Gloria esta en lista de espera para tomar el libro prestado
+        biblioteca.agregarEspera("BK-001", "Nicolas"); //luego sigue Nicolas y de ultimas Robinson
+        biblioteca.agregarEspera("BK-001", "Robinson");
+
+        System.out.println(biblioteca.siguienteEnEspera("BK-001"));
+        System.out.println(biblioteca.siguienteEnEspera("BK-001"));
+
+
+        System.out.println("Membresías:");
+        System.out.println(biblioteca.registrarMiembro("Gloria"));  //hashset no permite duplicados de usuarios
+        System.out.println(biblioteca.registrarMiembro("Nicolas")); //este se registro correctamente
+        System.out.println(biblioteca.registrarMiembro("SilvaMarce"));
+        System.out.println(biblioteca.registrarMiembro("Gloria"));  //intente registrar a Gloria de nuevo y me salio false
+        System.out.println(biblioteca.tieneMembresia("Nicolas"));  //da true, porque si tiene membresia
+        System.out.println(biblioteca.tieneMembresia("AngieFer")); //false porque AngieFer no tiene membresia
+}
 }
--- src/SistemaBiblioteca.java
+++ src/SistemaBiblioteca.java
@@ -1,9 +1,54 @@
-import java.util.HashMap; //HashMap coleccion
+
+import java.util.HashMap; //HashMap coleccion   busca materiales por codigo
 import java.util.Map;
+import java.util.ArrayList;  // ArrayList mantiene la fila de espera, mantener el orden de la fila
+import java.util.List;
+import java.util.HashSet;  // HashSet guarda miembros sin repetir, comprobar membresia
+import java.util.Set;
+
+
 
 public class SistemaBiblioteca {
     private Map<String, Material> catalogo = new HashMap<>();
     public void registrarMaterial(Material m) {
         catalogo.put(m.getCodigo(), m);
+    }
+
+    public Material buscarMaterial(String codigo) {
+        return catalogo.get(codigo);
+    }
+
+    public void mostrarCatalogo() {
+        for (Map.Entry<String, Material> entrada : catalogo.entrySet()) {
+            entrada.getValue().mostrarInfo();
+        }
+    }
+
+    public void agregarEspera(String codigoMaterial, String usuario) {
+
+        if (!listaEspera.containsKey(codigoMaterial)) {
+            listaEspera.put(codigoMaterial, new ArrayList<>());
+        }
+
+        listaEspera.get(codigoMaterial).add(usuario);  //agrega usuarios al final de la fila
+    }
+
+    public String siguienteEnEspera(String codigoMaterial) {
+        List<String> lista = listaEspera.get(codigoMaterial);
+        if (lista == null || lista.isEmpty()) {   //si la linea no existe o si la linea esta vacia
+            return null;  //devuelve null
+        }
+        return lista.remove(0); //lista.remove(0) saca al primer usuario que llegó de la fila
+    }
+
+    private Map<String, List<String>> listaEspera = new HashMap<>();
+    private Set<String> miembros = new HashSet<>();  //Set<String> no permite duplicados, osea que no se repitan los usuarios o miembros
+
+    public boolean registrarMiembro(String usuario) {
+        return miembros.add(usuario);  //add() devuelve false si se intenta colocar un usuario que ya existe
+    }
+
+    public boolean tieneMembresia(String usuario) {
+        return miembros.contains(usuario);  //contains() pregunta si el usuario esta dentro del conjunto, osea si un usuario tiene membresia y esta registrado devuelve true, de lo contrario devuelve false
     }
 }
